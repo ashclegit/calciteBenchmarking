308b23efdc934018e6bf420b0d76ca846097cf14
Seung-Hwan Lim <s52@nittany.ornl.gov>
2018-04-27 10:06:25 -0400

    add working queries

--- working/query95.sql
+++ working/query95.sql
@@ -1,29 +1,36 @@
+-- start query 95 in stream 0 using template query95.tpl
+-- \timing on
+
 with ws_wh as
-(select ws1."ws_order_number",ws1."ws_warehouse_sk" wh1,ws2."ws_warehouse_sk" wh2
-from "postgrestest"."web_sales" ws1,"postgrestest"."web_sales" ws2
-where ws1."ws_order_number" = ws2."ws_order_number"
-  and ws1."ws_warehouse_sk" <> ws2."ws_warehouse_sk")
-select
-  count(distinct ws."ws_order_number") as "order count"
- ,sum(ws."ws_ext_ship_cost") as "total shipping cost"
- ,sum(ws."ws_net_profit") as "total net profit"
+(select ws1."ws_order_number",ws1."ws_warehouse_sk" WH1,ws2."ws_warehouse_sk" WH2
+ from "web_sales" ws1,"web_sales" ws2
+ where ws1."ws_order_number" = ws2."ws_order_number"
+   and ws1."ws_warehouse_sk" <> ws2."ws_warehouse_sk")
+ select  
+   count(distinct "ws_order_number") as "order count"
+  ,sum("ws_ext_ship_cost") as "total shipping cost"
+  ,sum("ws_net_profit") as "total net profit"
 from
-  "postgrestest"."web_sales" ws
- ,"postgrestest"."date_dim" d
- ,"postgrestest"."customer_address" ca
- ,"postgrestest"."web_site" web
+   "postgrestest"."web_sales" ws
+  ,"postgrestest"."date_dim" d
+  ,"postgrestest"."customer_address" ca
+  ,"postgrestest"."web_site" w
 where
-   d."d_date" between '1999-5-01' and
-          (cast('1999-5-01' as date) + INTERVAL '60' day)
-and ws."ws_ship_date_sk" = d."d_date_sk"
-and ws."ws_ship_addr_sk" = ca."ca_address_sk"
-and ca."ca_state" = 'MT'
-and ws."ws_web_site_sk" = web."web_site_sk"
-and web."web_company_name" = 'pri'
-and ws."ws_order_number" in (select ws."ws_order_number"
-                           from ws_wh)
-and ws."ws_order_number" in (select wr."wr_order_number"
-                           from "postgrestest"."web_returns" wr,ws_wh 
-                           where wr."wr_order_number" = ws_wh."ws_order_number")
-order by count(distinct ws."ws_order_number")
-limit 10;
+    "d_date" between '1999-5-01' and (cast('1999-6-30' as date))
+--           (cast('1999-5-01' as date) + interval '60' day)
+and ws."ws_ship_date_sk" = "d_date_sk"
+and ws."ws_ship_addr_sk" = "ca_address_sk"
+and "ca_state" = 'MT'
+and ws."ws_web_site_sk" = "web_site_sk"
+and "web_company_name" = 'pri'
+and ws."ws_order_number" in (select "ws_order_number"
+                            from ws_wh)
+and ws."ws_order_number" in (select "wr_order_number"
+                            from "web_returns",ws_wh
+                            where "wr_order_number" = ws_wh."ws_order_number")
+order by count(distinct "ws_order_number")
+limit 100;
+
+-- \timing off
+-- end query 95 in stream 0 using template query95.tpl
+-- done
